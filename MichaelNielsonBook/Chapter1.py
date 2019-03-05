a17355fc2f5afe010b2983293f0bc10495a64147
reachadityahere <a@b.com>
2019-03-05 13:31:29 +0530

    V1

--- MichaelNielsonBook/Chapter1.py
+++ MichaelNielsonBook/Chapter1.py
@@ -47,11 +47,16 @@ class Network(object):
 
     def feedforward(self, a):
         #n - 1 matrices for n layers because input layer has no weights and biases
-        for current_layer in range(self.num_layers - 1):
-            print(a.shape)
-            z = self.weights[current_layer].dot(a) + self.biases[current_layer]
-            z = sigmoid(z)
-        return z
+
+        #TODO BUGGY Implementation
+        # for current_layer in range(self.num_layers - 1):
+        #     print(a.shape)
+        #     z = self.weights[current_layer].dot(a) + self.biases[current_layer]
+        #     z = sigmoid(z)
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, a)+b
+            a = sigmoid(z)
+        return (a, z)
 
     def update_mini_batch(self, mini_batch, alpha):
         delta_w = np.zeros_like(self.weights)
@@ -71,9 +76,6 @@ class Network(object):
 
 
     def SGD(self, training_data, epochs, mini_batch_size, learning_rate, test_data = None):
-        '''
-        training_data is rows of tuples(x,y)
-        '''
         if test_data:
             number_tests = len(test_data)
 
@@ -90,64 +92,50 @@ class Network(object):
             else:
                 print("Epoch {0} finished".format(e))
 
-####################################################################################################################################
     def backprop(self, x, y):
-        """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
-        nabla_b = np.zeros_like(self.biases)
-        nabla_w = np.zeros_like(self.weights)
-        # feedforward
-        activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
-        for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
-            zs.append(z)
-            activation = sigmoid(z)
-            activations.append(activation)
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_derivative(zs[-1])
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        # Note that the variable l in the loop below is used a little
-        # differently to the notation in Chapter 2 of the book.  Here,
-        # l = 1 means the last layer of neurons, l = 2 is the
-        # second-last layer, and so on.  It's a renumbering of the
-        # scheme in the book, used here to take advantage of the fact
-        # that Python can use negative indices in lists.
-        for l in range(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_derivative(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+    	temp_b = np.zeros_like(self.biases)
+    	temp_w = np.zeros_like(self.weights)
+
+    	activation = x
+    	activation_storage = [x]
+    	z_storage = []
+
+    	for w, b in zip(self.weights, self.biases):
+    		z = w.dot(activation) + b
+    		z_storage.append(z)
+    		activation = sigmoid(z)
+    		activation_storage.append(activation)
+
+    	#Computes error at output layer
+    	delta = self.cost_derivative(activation_storage[-1], y) * sigmoid_derivative(z_storage[-1])
+    	temp_b[-1] = delta
+    	temp_w[-1] = delta.dot(activation_storage[-2].transpose())
+
+    	#back-propagates error to earlier layers
+    	for layer in range(2, self.num_layers):
+    		z = z_storage[-layer]
+    		sp = sigmoid_derivative(z)
+    		delta = np.dot(self.weights[-layer+1].transpose(), delta) * sp
+    		temp_b[-layer] = delta
+    		temp_w[-layer] = delta.dot(activation_storage[-layer-1].transpose())
+
+    	return (temp_b, temp_w)
 
     def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feedforward(x)), y)
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
+    	test_results = [(np.argmax(self.feedforward(x)[0]), y) for (x, y) in test_data]
+    	return sum(int(x==y) for (x, y) in test_results)
 
-    def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives partial C_x /
-            partial a for the output activations."""
-        return (output_activations-y)
+    def cost_derivative(self, output_activation_storage, y):
+    	return (output_activation_storage-y)
 
-####################################################################################################################################
 
 training_data, validation_data, test_data = load_mnist_data()
 test_data = list(test_data)
 training_data = list(training_data)
 validation_data = list(validation_data)
 net = Network([784, 30, 10])
-# net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
-net.SGD(training_data, 30, 10, 3.0, test_data = training_data)
+# net.SGD(training_data, 30, 10, 3.0)
+net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
 print("test")
             #TODO: Change initialization to random.random
             #TODO: Play with mini_batch_size, set as 1
